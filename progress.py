e16086788ecb04181336a2a9aaa924d40ea88e87
elipereiradev <58385831+elipereiradev@users.noreply.github.com>
2022-12-12 18:47:42 +0000

    first commit

---
+++ progress.py
@@ -0,0 +1,7 @@
+name = input('Entry you name: ')
+age = input('Entry your age: ')
+weight = input('Entry your weight: ')
+
+print('Yor name is ' + name + '. Nice to meet you!')
+print('Your Age is ' + age + ' ,write?')
+print('Your Weight is ' + weight)
