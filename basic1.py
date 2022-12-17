bacac7d6cbade7f48cfc1812e13d5cf99a78b143
elipereiradev <58385831+elipereiradev@users.noreply.github.com>
2022-12-17 13:23:44 +0000

    Start Project1 with Flask and created the first page index.html

--- basic1.py
+++ basic1.py
@@ -1,36 +1,22 @@
-#lists with python
+""" class Elevator:
+    def __init__(self, starting_floor):
+        self.make = 'The elevator company'
+        self.floor = starting_floor
+        
+# To create the object
 
-""" list1 = [1,2,3,4]
-list2 = ['a', 'b', 'c']
-list3 = [True, False, True]
-list4 = [1, 'a', True]
-numbers = [1,2,3,4,5,6,7,8,9]
-quadrados_impares = [numbers ** 2 for numbers in numbers if numbers % 2 == 1]
-quadrados_pares = [numbers ** 2 for numbers in numbers if numbers % 2 == 0] """
+elevator = Elevator(1)
+print(elevator.make)
+print(elevator.floor) """
 
+# The wrong form of "self"
+class Car:
+    def __init__(self, Hybrid):
+        self.tipo = "Hybrid"
+        self.color = "Red" # ends up on the object
+        self.make = "Mercedes" # becomes a local variable in the constructor
 
-#for multi_list in list1:
-#  print(multi_list **2)
-
-#print(list1[0], list2[0], list3[0], list4[0])
-
-#print(quadrados_impares)
-#print(quadrados_pares)
-
-""" def anexa_correto(elem, lista=None):
-    if not lista:
-        lista = []
-    lista.append(elem)
-    return lista """
-
-""" a, b = 40, 50
-print(a, b) """
-
-def foo(a,b, c, **args, **kwargs):
-    print('a: {} {}'.format(a, type(a)))
-    print('b: {} {}'.format(b, type(b)))
-    print('c: {} {}'.format(c, type(c)))
-    print('args: {} {}'.format(args, type(args)))
-    print('kwargs: {} {}'.format(kwargs, type(kwargs)))
-
-print foo(1,2,3)
+car = Car(2)
+print(car.tipo) #Hybrid
+print(car.color) # "Red"
+print(car.make) # would result in an error, `make` does not exist on the object
